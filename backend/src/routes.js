ad9916caa05a0dc8049ac96e8188998085e5a80b
swyssauro <xspynni23@gmail.com>
2020-03-24 12:49:23 -0300

    🐱‍🚀 update controllers

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,27 +1,8 @@
 const express = require('express');
-const crypto = require('crypto');
+const OngController = require('./controllers/OngController');
 const routes = express.Router();
-const connection = require('./database/connection');
 
-routes.get('/ongs', async (request, response) => {
-    const ongs = await connection('ongs').select('*');
-    return response.json(ongs);
-});
-
-routes.post('/ongs', async (request, response) => {
-    const { name, email, whatsaap, city, uf } = request.body;
-    const id = crypto.randomBytes(4).toString('HEX');
-
-    await connection('ongs').insert({
-        id,
-        name,
-        email,
-        whatsaap,
-        city,
-        uf,
-    })
-
-    return response.json({ id });
-});
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
 
 module.exports = routes;
