e3b522fcc56e234d759bda739b6d4d562377aec0
swyssauro <xspynni23@gmail.com>
2020-03-25 11:21:23 -0300

    😨 update controllers

--- backend/src/controllers/incidentController.js
+++ backend/src/controllers/incidentController.js
@@ -3,7 +3,25 @@ const connection = require('../database/connection');
 module.exports = {
 
     async index(resquest, response) {
-        const incidents =  await connection('incidents').select('*');
+        const { page = 1 } = resquest.query;
+
+        const [count] = await connection('incidents').count();
+
+        const incidents =  await connection('incidents')
+            .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+            .limit(5)
+            .offset((page - 1) * 5)
+            .select([
+                'incidents.*', 
+                'ongs.name', 
+                'ongs.email', 
+                'ongs.whatsaap', 
+                'ongs.city', 
+                'ongs.uf'
+            ]);
+
+        response.header('X-Total-Count', count['count(*)']);
+
         return response.json(incidents);
     },
 
